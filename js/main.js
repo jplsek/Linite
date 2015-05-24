9df181bfd0802a9521640ff54ddf9adacbbdde71
Jeremy Plsek <jeremyplsek@gmail.com>
2015-05-23 22:55:49 -0400

    main features work now. side project done. time to add everything and make it look nice.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,152 @@
 // Main js file.
+
+// All info
+var apps = null;
+
+// Generated info
+var distro = null;
+var appList = [];
+
+// load apps.json, with a deferred object.
+// seems to not work properly in chrom*
+// loading the file in firefox gives "not well-formed", for valid json.
+function getapps(d1) {
+
+    var json = null;
+
+    $.ajax({
+
+        'url': "js/apps.json",
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+
+    }).done(function() {
+        apps = json;
+        d1.resolve(json);
+    });
+}
+
+// run this after the apps.json is loaded
+function main() {
+    console.log(apps);
+}
+
+function addDistro() {
+    $("#apps").empty();
+    if (distro == "debian")
+        $("#apps").append("sudo apt-get install ");
+    if (distro == "fedora")
+        $("#apps").append("sudo yum install ");
+}
+
+function appendAppList() {
+    console.log("test");
+    addDistro();
+    for (app in appList) {
+        console.log(app);
+        $("#apps").append(appList[app] + " ");
+    }
+}
+
+$(document).ready(function() {
+    var d1 = $.Deferred();
+    getapps(d1);
+
+    $.when(d1).done(function() {
+        main();
+    });
+});
+
+// assuming apps is generated
+// generates the application picker
+$("#distro-choice input").click(function() {
+
+    // remove all elements in app-choice before generating
+    $("#app-choice").empty();
+
+    distro = this.value
+
+    console.log("Distro: " + distro);
+
+    addDistro();
+
+    var types = [];
+
+    console.log("Categories: " + apps.global.length);
+
+    // generate the categories
+    for (var i = 0; i < apps.global.length; i++) {
+
+        var typeG = apps.global[i]; 
+        var typeD = apps[distro][i];
+        types.push($.extend(true, typeG, typeD));
+        //console.log(obj[distro]);
+    }
+
+    //var test = apps.global[0].browsers[0].name;
+    //console.log(test);
+
+    console.log(types); 
+
+    // need to optimize this...
+    // 0. selects 1st array
+    $.each(types, function (array, category) {
+        // 1. selects category
+        $.each(category, function (categoryName, subObject) {
+            console.log(categoryName);
+            html = "<div class=\"category\"><h4>" + categoryName + "</h4>";
+            // 2. selects 1st array item
+            $.each(subObject, function (array, subObject) {
+                //console.log("2: " + subI + "=" + subObject);
+                // 3. selects app
+                $.each(subObject, function (uniqueName, subObject) {
+                    //console.log("3: " + subI + "=" + subObject);
+                    var appName = null;
+                    var appIcon = null;
+                    var appProg = null;
+                    // 4. selects app values
+                    $.each(subObject, function (name, value) {
+                        console.log("4: " + name + "=" + value);
+
+                        switch(name) {
+                            case "name":
+                                appName = value;
+                                break;
+                            case "icon":
+                                appImg = value;
+                                break;
+                            case "program":
+                                appProg = value;
+                                break;
+                        }
+                    });
+
+                    html += "<label><input type=\"checkbox\" name=\"appProg\" value=\"" + appProg + "\"> ";
+                    html += "<img src=\"img/" + appImg + "\" alt=\"\">";
+                    html += "<span>" + appName + "</span></label>";
+                });
+            });
+
+            html += "</div>";
+            $("#app-choice").append(html);
+        });
+    });
+});
+
+$("body").on("click", ".category input", function(){
+
+    appList = [];
+
+    $("input[name='appProg']").each(function() {
+
+        if ($(this).is(":checked")) {
+            console.log(this.value);
+            appList.push(this.value);
+        }
+    });
+
+    console.log(appList);
+    appendAppList();
+});
--- readme.md
+++ readme.md
@@ -9,12 +9,7 @@ As if current package managers aren't easy enough, this will stream line the pro
 I will be working on this when my partners don't need me to work on stuff for them. This project shouldn't take too long, which is why it will be a side project on CodeDay itself.
 (In case you're wondering, they are making a wallpaper generator. I will be creating an (optional) interface for them, while they create the backend.)
 
-# NOTE 
-This readme (the init commit) was written BEFORE CodeDay, and parts of it (plan, script layout, etc) might be removed after all of this is implemented.
-
 ## Plan
-
-I will probably use bootstrap as a quick layout setup, and change it as I need to.
 
 1. Options to choose distribution.
     * This is used to set the package manager. (yum, apt-get, pacman, etc)
@@ -26,64 +21,6 @@ I will probably use bootstrap as a quick layout setup, and change it as I need t
     * Script will be highlighted to allow copying with ctrl+c. (No Flash will be used for an easy "copy" button.)
     * There will be an option to save the script.
     * Could also put a "yes" check box to add, for example "-y" to apt-get.
-
-## Possible script layout
-
-* A global application list, where the application is the same across all distributions. 
-* A distribution application list, where the application name is different across distributions.
-For example:
-```javascript
-{
-    "global": {
-        "browsers": {
-            "name":"Web Browser",
-            "chromium": {
-                "name":"Chromium",
-                "package":"chromium-browser",
-                "icon":"chromium.jpg",
-            },
-        },
-        "programming": {
-            "name":"Programming Tools",
-            "pyton3-dev": {
-                "name":"Python 3 Development",
-                "icon":"python.jpg",
-            },
-        },
-    },
-    "debian":{
-        "browsers": {},
-        "programming": {
-            "python3-dev": {
-                "package":"python3-dev",
-            },
-    },
-    "fedora":{
-        "browsers": {},
-        "programming": {
-            "python3-dev": {
-                "package":"python3-devel",
-            },
-        },
-    },
-}
-```
-
-Choosing "Debian" from the interface list will generate the list from the "global" and "debian" arrays.
-For example:
-```javascript
-$("#distro-choice").click(function() {
-    // hopefully this loop will work like I'm thinking
-    for category in json.global {
-        types[] = $.extend(true, category, json.this.value[i]); // this is assuming that the categories will be in the same order
-        i++;
-    }
-    for type in types[] {
-        // do thing here to generate the list
-        // similar to ninite: categories are bolded, apps underneath category, maybe after every 2 categories, make a new column
-    }
-});
-```
 
 ## Possible future features
 
