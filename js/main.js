e68c9a1357117cb9ff13ad7f0b3de01ad840b988
jplsek <jeremyplsek@gmail.com>
2015-05-25 14:08:27 -0400

    put each distro into their own file, rearranged js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,37 @@
 // Main js file.
 
-// All info
-var apps = null;
+// global app information
+var global = null;
 
-// Generated info
-var distro = "ubuntu"; // set default distro
-var appList = [];
+var selectedDistro = null;
 
 // load apps.json, with a deferred object.
 // seems to not work properly in chrom*
 // loading the file in firefox gives "not well-formed", for valid json.
-function getapps(d1) {
+function getapps(d1, distro) {
+
+    addDistro(distro);
 
     var json = null;
+    var file = "distro/" + distro + ".json";
+
+    console.log("Getting file " + file);
 
     $.ajax({
 
-        'url': "js/apps.json",
-        'dataType': "json",
-        'success': function (data) {
+        "url": file,
+        "dataType": "json",
+        "success": function (data) {
             json = data;
         }
 
     }).done(function() {
-        apps = json;
         d1.resolve(json);
     });
 }
 
-// run this after the apps.json is loaded.
-function main() {
-    console.log(apps);
-    $("#ubuntu-select").prop("checked", true);
-    distroSelect();
-}
-
 // set up package manager here. Probably should implement this into the json.
-function addDistro() {
+function addDistro(distro) {
     $("#apps").empty();
     if (distro == "ubuntu" || distro == "debian")
         $("#apps").append("sudo apt-get install");
@@ -44,86 +39,93 @@ function addDistro() {
         $("#apps").append("sudo pacman -S");
     else if (distro == "fedora")
         $("#apps").append("sudo yum install");
+    else if (distro == "global")
+        console.log("global selected");
     else
-        console.log("Distro not supported, please implement.");
+        console.log("Distro: " + distro + " not supported, please implement.");
 }
 
 // assumes distro is set.
-// generates the category list
-function distroSelect() {
+// generates the category list, apps referes to a json
+function distroSelect(distro) {
 
-    // remove all elements in app-choice before generating
-    $("#app-choice").empty();
+    selectedDistro = distro;
 
-    console.log("Distro: " + distro);
+    var d = $.Deferred();
+    getapps(d, distro);
 
-    addDistro();
+    $.when(d).done(function(apps) {
 
-    var types = [];
+        console.log("done getting distro: " + distro + ", generating apps");
 
-    console.log("Categories: " + apps.global.length);
+        // remove all elements in app-choice before generating
+        $("#app-choice").empty();
 
-    // generate the categories
-    for (var i = 0; i < apps.global.length; i++) {
+        //console.log("Distro: " + distro);
 
-        var typeG = apps.global[i]; 
-        var typeD = apps[distro][i];
-        types.push($.extend(true, typeG, typeD));
-        //console.log(obj[distro]);
-    }
+        var types = [];
 
-    //var test = apps.global[0].browsers[0].name;
-    //console.log(test);
+        //console.log("Categories: " + apps.global.length);
 
-    console.log(types); 
+        // generate the categories
+        for (var i = 0; i < global.length; i++) {
 
-    // need to optimize this...
-    // 0. selects 1st array
-    $.each(types, function (array, category) {
-        // 1. selects category
-        $.each(category, function (categoryName, subObject) {
-            //console.log(categoryName);
-            html = "<div class=\"category col-xs-12 col-sm-3\"><h4>" + categoryName + "</h4>";
-            // 2. selects 1st array item
-            $.each(subObject, function (array, subObject) {
-                //console.log("2: " + subI + "=" + subObject);
-                // 3. selects app
-                $.each(subObject, function (uniqueName, subObject) {
-                    //console.log("3: " + subI + "=" + subObject);
-                    var appName = null;
-                    var appIcon = null;
-                    var appProg = null;
-                    // 4. selects app values
-                    $.each(subObject, function (name, value) {
-                        //console.log("4: " + name + "=" + value);
+            var categoryGlobal = global[i]; 
+            var categoryDistro = apps[i];
+            types.push($.extend(true, categoryGlobal, categoryDistro));
+        }
 
-                        switch(name) {
-                            case "name":
-                                appName = value;
-                                break;
-                            case "icon":
-                                appImg = value;
-                                break;
-                            case "program":
-                                appProg = value;
-                                break;
-                        }
+        console.log(types); 
+
+        // need to optimize this...
+        // 0. selects 1st array
+        $.each(types, function (array, category) {
+            // 1. selects category
+            $.each(category, function (categoryName, subObject) {
+                //console.log(categoryName);
+                html = "<div class=\"category col-xs-12 col-sm-3\"><h4>" + categoryName + "</h4>";
+                // 2. selects 1st array item
+                $.each(subObject, function (array, subObject) {
+                    //console.log("2: " + subI + "=" + subObject);
+                    // 3. selects app
+                    $.each(subObject, function (uniqueName, subObject) {
+                        //console.log("3: " + subI + "=" + subObject);
+                        var appName = null;
+                        var appIcon = null;
+                        var appProg = null;
+                        // 4. selects app values
+                        $.each(subObject, function (name, value) {
+                            //console.log("4: " + name + "=" + value);
+
+                            switch(name) {
+                                case "name":
+                                    appName = value;
+                                    break;
+                                case "icon":
+                                    appImg = value;
+                                    break;
+                                case "program":
+                                    appProg = value;
+                                    break;
+                            }
+                        });
+
+                        html += "<label><input type=\"checkbox\" name=\"appProg\" value=\"" + appProg + "\"> ";
+                        html += "<img src=\"img/" + appImg + "\" alt=\"\">";
+                        html += "<span>" + appName + "</span></label><br>";
                     });
-
-                    html += "<label><input type=\"checkbox\" name=\"appProg\" value=\"" + appProg + "\"> ";
-                    html += "<img src=\"img/" + appImg + "\" alt=\"\">";
-                    html += "<span>" + appName + "</span></label><br>";
                 });
-            });
 
-            html += "</div>";
-            $("#app-choice").append(html);
+                html += "</div>";
+                $("#app-choice").append(html);
+            });
         });
     });
 }
 
-function appendAppList() {
-    addDistro();
+function appendAppList(appList) {
+    addDistro(selectedDistro);
+
     for (app in appList) {
         console.log(app);
         $("#apps").append(" " + appList[app]);
@@ -131,11 +133,19 @@ function appendAppList() {
 }
 
 $(document).ready(function() {
-    var d1 = $.Deferred();
-    getapps(d1);
 
-    $.when(d1).done(function() {
-        main();
+    // select ubuntu radio in case it's cached, may change later
+    $("#ubuntu-select").prop("checked", true); 
+
+    // generate the global first
+    var d = $.Deferred();
+    getapps(d, "global");
+
+    $.when(d).done(function(globalapps) {
+        
+        console.log("done getting global");
+        global = globalapps;
+        distroSelect("ubuntu");
     });
 
 });
@@ -143,13 +153,13 @@ $(document).ready(function() {
 // assuming apps is generated
 // generates the application picker
 $("#distro-choice input").click(function() {
-    distro = this.value;
-    distroSelect();
+    distroSelect(this.value);
 });
 
+// added the app to the textarea
 $("body").on("click", ".category input", function() {
 
-    appList = [];
+    var appList = [];
 
     $("input[name='appProg']").each(function() {
 
@@ -159,10 +169,11 @@ $("body").on("click", ".category input", function() {
         }
     });
 
-    console.log(appList);
-    appendAppList();
+    //console.log(appList);
+    appendAppList(appList);
 });
 
+// highlights the textarea
 $("#apps").click(function() {
     this.select();
 })
--- readme.md
+++ readme.md
@@ -1,13 +1,10 @@
 Linite
 ======
 
-This web-application will be started on CodeDay, Spring 2015.
+You can view it here: [http://jplsek.github.io/Linite/](http://jplsek.github.io/Linite/)
 
 This is meant to be similar to the free version of [Ninite](https://ninite.com/), except to create just a simple script to run.
 As if current package managers aren't easy enough, this will stream line the process even more (specifically for those distro-hoppers ;) ).
-
-I will be working on this when my partners don't need me to work on stuff for them. This project shouldn't take too long, which is why it will be a side project on CodeDay itself.
-(In case you're wondering, they are making a wallpaper generator. I will be creating an (optional) interface for them, while they create the backend.)
 
 ## Plan
 
@@ -22,10 +19,11 @@ I will be working on this when my partners don't need me to work on stuff for th
     * There will be an option to save the script.
     * Could also put a "yes" check box to add, for example "-y" to apt-get.
 
-## Bugs
+## Note to developers
 
-* Due to a security feature in chromium / google chrome, this doesn't work.
-* Firefox gives a warning saying the json is "not well-formed", when it seems correct.
+* When using this without a web server:
+	* Due to a security feature in chromium / google chrome, this doesn't work.
+	* Firefox gives a warning saying the json has a "syntax error", when it seems correct.
 
 ## Possible future features
 
@@ -37,7 +35,6 @@ I will be working on this when my partners don't need me to work on stuff for th
 
 ## Other
 
-* Ninite for Linux did exist in the past. They currently don't want to support it for obvious reasons. (aka package management in Linux is easy enough, and their business model if pretty much a package manager for Windows.)
-* Someone else has probably done this same exact thing.
-* If you're on Windows, and want a "package manager" that isn't Ninite, check out chocolatey, it's free.
-* Contribute if you want.
+* Contribute/Pull if you want.
+* This web application was started in CodeDay Boston, Spring 2015.
+* This uses the MIT License (MIT).
